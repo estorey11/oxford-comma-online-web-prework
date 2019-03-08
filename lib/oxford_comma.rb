71c5bb4c1f5767b6bb4ddf00d52444521a700667
Eric Storey <estorey@gmail.com>
2019-03-08 22:38:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,11 +6,13 @@ def oxford_comma(array)
     str << array.shift
     str << " and "
     str << array.shift
-  array.each_with_index do |word, i|
-    if i + 1 < array.length 
-      str << "#{word}, "
-    else 
-      str << "and #{word}"
+  else
+    array.each_with_index do |word, i|
+      if i + 1 < array.length 
+        str << "#{word}, "
+      else 
+        str << "and #{word}"
+      end
     end
   end
   str
